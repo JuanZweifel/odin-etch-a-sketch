9573f15ab34592a1500c6070df20a78c16f0447f
JuanZweifel <jua.borquez@duocuc.cl>
2024-02-20 18:22:22 -0300

    Squares with borders

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 const container = document.querySelector("#container")
 const resetButton = document.querySelector("#reset")
+const containerWidth = container.clientWidth
 
 gridGenerator(16)
 
 function gridGenerator(n) {
 	container.replaceChildren();
-	let squareWidthHeight = (400/n)+"px"
+	let squareWidthHeight = (containerWidth/n)
 	for (let index = 0; index < n*n; index++) {
 		const square = document.createElement('div');
-		square.style.color = "black"
-		square.style.height = squareWidthHeight
-		square.style.width = squareWidthHeight
+		let borderOffset = 2
+		square.style.height = (squareWidthHeight - borderOffset)+"px"
+		square.style.width = (squareWidthHeight - borderOffset)+"px"
+		square.style.border = "black solid 1px"
 		container.appendChild(square);
 	}
 	const squares = document.querySelectorAll('#container>div');
